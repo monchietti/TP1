8373abf81917a7acc9c9458c206bd2edefd87979
franm <franmonchietti@gmail.com>
2025-09-18 15:59:23 -0300

    implementacion de humedad

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,11 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 // ---- Variables globales ----
 uint8_t screenIdx = 0; // pantalla actual
 unsigned long lastBtnMs = 0; 
-const unsigned long DEBOUNCE_MS = 40; 
+const unsigned long DEBOUNCE_MS = 200; 
+float humedadMinimaDeseada = 0.0;    // valor inicial aleatorio
+float humedadActual = dht.readHumidity();    // última humedad mostrada
+boolean ventilacionEncendida = false;
+boolean riegoEncendido = false;
 
 
 //---- Prototipos de funciones ----
@@ -51,6 +55,13 @@ void setup() {
   // inicialización pines
   pinMode(PIN_LED, OUTPUT);
   pinMode(ENC_SW, INPUT_PULLUP); // botón del encoder
+
+  // Inicializar generador de aleatorios
+  randomSeed(analogRead(0));
+
+  // Generar umbral inicial entre 40 y 60
+  humedadMinimaDeseada = random(40, 61);
+  humedadActual = dht.readHumidity();
 }
 
 void loop() {
@@ -72,32 +83,56 @@ void escribirPantalla(String texto){
 
 void showScreenTemp() {
   // Leer sensor
-   int valor = analogRead(PIN_POT);
-   float tempC_pot = mapPotToTempC(valor);
-   Serial.println(tempC_pot);
-
+  int valor = analogRead(PIN_POT);
+  float tempC_pot = mapPotToTempC(valor);
   float tempC = dht.readTemperature(); // en Celsius
-  Serial.println(tempC);
   float hum   = dht.readHumidity();
   String strHumedad = String(hum, 2); 
   String texto = "Pantalla Temperatura \n Temp: " + String(tempC, 2) + " C\n Temp Pot: " + String(tempC_pot, 2) + " C";
   
   if(tempC > tempC_pot){
-    Serial.println("en pantalla ventilacion");
     digitalWrite(PIN_LED, HIGH);
     escribirPantalla(texto + "\n Ventilacion ON");
+    
+    if (!ventilacionEncendida){
+      ventilacionEncendida = true;
+      Serial.println("Ventilacion encendida");
+    }
+
   } else {
     digitalWrite(PIN_LED, LOW);
     escribirPantalla(texto + "\n Ventilacion OFF");
-  }
+    if (ventilacionEncendida){
+      ventilacionEncendida = false;
+      Serial.println("Ventilacion apagada");
+    }
 
-  delay(500);  // refresco cada 1 s
+  }
 
 }
 
 void showScreenHum() {
-  escribirPantalla("Pantalla Humedad");
-  delay(1000);  // refresco cada 1 s
+  humedadActual = dht.readHumidity();
+  String textoRiego = "Humedad min deseada:\n" + String(humedadMinimaDeseada, 0) + "%\n\n" + "Humedad actual:\n" + humedadActual + "%";
+
+  if(humedadActual < humedadMinimaDeseada) {
+    digitalWrite(PIN_LED, HIGH);
+    delay(200);
+    digitalWrite(PIN_LED, LOW);
+    delay(200);
+    textoRiego += "\n\n Riego Encendido";
+    if (!riegoEncendido){
+      riegoEncendido = true;
+      Serial.println("Riego encendido");
+    }
+  }else{
+    textoRiego += "\n\n Riego Apagado";
+    if(riegoEncendido){
+      riegoEncendido = false;
+      Serial.println("Riego apagado");
+    }
+  }
+  escribirPantalla(textoRiego);
 }
 
 void handleEncoderButton() {
@@ -106,7 +141,7 @@ void handleEncoderButton() {
   if (digitalRead(ENC_SW) == LOW) {       // INPUT_PULLUP: presionado = LOW
     lastBtnMs = now;
     screenIdx = (screenIdx + 1) % 2;
-    Serial.printf("[UI] Cambio a Pantalla", screenIdx + 1 + "\n");
+    Serial.println("[UI] Cambio a Pantalla"+ String(screenIdx + 1));
     // refrescar inmediatamente y aplicar modo de LED de la pantalla
   }
 }
